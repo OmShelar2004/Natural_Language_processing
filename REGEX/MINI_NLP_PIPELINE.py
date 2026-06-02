3b0c97722d3a998a4adba7fdba4f8c161bc7eef7
OmShelar2004 <omshelar32004@gmail.com>
2026-06-03 00:53:12 +0530

    Add REGEX directory with copies of NLP files

---
+++ REGEX/MINI_NLP_PIPELINE.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import string
+
+text = "I AM Learning NLP!!! and it is AMAZING 123"
+
+# Lowercase
+text = text.lower()
+
+# Remove punctuation
+translator = str.maketrans('', '', string.punctuation)
+text = text.translate(translator)
+
+# Tokenization
+words = text.split()
+
+# Remove numbers
+words = [word for word in words if not word.isdigit()]
+
+# Remove stopwords
+stopwords = ["i", "am", "and", "it", "is"]
+
+words = [word for word in words if word not in stopwords]
+
+print(words)
